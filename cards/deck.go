9c303c6148b8a65ac1b36975cd7afc67b4f560bf
NateHan <nathan.hanak@formation.ai>
2020-01-23 14:11:18 -0600

    further progress with deck example

--- cards/deck.go
+++ cards/deck.go
@@ -5,8 +5,29 @@ import "fmt"
 // deck now inherits all functionalities of a string slice
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Clubs", "Hearts"}
+	cardValues := []string{"Ace", "Two", "Three","Four","Five",
+	"Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
