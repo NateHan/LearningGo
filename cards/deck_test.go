abddf92e80710741d1774d475c8dd3049cf65370
NateHan <nathan.hanak@formation.ai>
2020-02-16 17:17:00 -0600

    Finished section three and brief example

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -2,6 +2,8 @@ package main
 
 import "testing"
 
+import "os"
+
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
@@ -11,4 +13,31 @@ func TestNewDeck(t *testing.T) {
 	if deckLen != expectedLength {
 		t.Errorf("Expected deck length of %v, but got %v", expectedLength, deckLen)
 	}
+
+	ex := "Ace of Spades"
+	if d[0] != ex {
+		t.Errorf("Expected %v but got %v", ex, d[0])
+	}
+
+	exEnd := "King of Hearts"
+	if d[len(d)-1] != exEnd {
+		t.Errorf("Expected %v but got %v", exEnd, d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	fileName := "_decktesting"
+	os.Remove(fileName)
+
+	d := newDeck()
+	d.saveToFile(fileName)
+
+	loadedDeck := newDeckFromFile(fileName)
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected a deck of 52 cards but got one of %v", len(loadedDeck))
+	}
+
+	os.Remove(fileName)
+
 }
